471e9715ae085bedf92aacdee8282ee5b7f3a279
roalnd <roland@buzzni.com>
2013-11-12 01:16:23 +0900

    init

---
+++ src/main/java/ScalaHelloWorld.scala
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: yd
+ * Date: 13. 11. 9
+ * Time: 오후 11:34
+ * To change this template use File | Settings | File Templates.
+ */
+
+object ScalaHelloWorld{
+  def main(args: Array[String]) {
+    print("HelloWorld")
+  }
+}
